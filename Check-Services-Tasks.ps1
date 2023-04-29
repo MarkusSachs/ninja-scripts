eb309e091d9dc5e2fa9952a2baf28e7b23b24763
Markus Sachs <markus.sachs@ametras.com>
2023-04-29 09:44:20 +0200

    added new Services

--- Check-Services-Tasks.ps1
+++ Check-Services-Tasks.ps1
@@ -102,8 +102,195 @@ If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
     Write-Host Service $serviceName not found
 }
 
-# Check RemotApp und Desktopverbindungsverwaltung
-$serviceName = 'TScPubRPC'
+# Check RDMS
+$serviceName = 'RDMS'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    $status = Get-Service -Name $serviceName
+    #Write-Output $status
+
+    if ( $status.Status -ne "Running") {
+        Write-Output "Service $serviceName nor running"
+        Start-Service -DisplayName '$serviceName'
+    } else {
+        Write-Output "Service $serviceName is running nothig to do"
+    }
+} else {
+    Write-Host Service $serviceName not found
+}
+
+# Check SessionEnv
+$serviceName = 'SessionEnv'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    $status = Get-Service -Name $serviceName
+    #Write-Output $status
+
+    if ( $status.Status -ne "Running") {
+        Write-Output "Service $serviceName nor running"
+        Start-Service -DisplayName '$serviceName'
+    } else {
+        Write-Output "Service $serviceName is running nothig to do"
+    }
+} else {
+    Write-Host Service $serviceName not found
+}
+
+# Check TermService
+$serviceName = 'TermService'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    $status = Get-Service -Name $serviceName
+    #Write-Output $status
+
+    if ( $status.Status -ne "Running") {
+        Write-Output "Service $serviceName nor running"
+        Start-Service -DisplayName '$serviceName'
+    } else {
+        Write-Output "Service $serviceName is running nothig to do"
+    }
+} else {
+    Write-Host Service $serviceName not found
+}
+
+# Check TermServLicensing
+$serviceName = 'TermServLicensing'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    $status = Get-Service -Name $serviceName
+    #Write-Output $status
+
+    if ( $status.Status -ne "Running") {
+        Write-Output "Service $serviceName nor running"
+        Start-Service -DisplayName '$serviceName'
+    } else {
+        Write-Output "Service $serviceName is running nothig to do"
+    }
+} else {
+    Write-Host Service $serviceName not found
+}
+
+# Check EFR
+$serviceName = 'EFR'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    $status = Get-Service -Name $serviceName
+    #Write-Output $status
+
+    if ( $status.Status -ne "Running") {
+        Write-Output "Service $serviceName nor running"
+        Start-Service -DisplayName '$serviceName'
+    } else {
+        Write-Output "Service $serviceName is running nothig to do"
+    }
+} else {
+    Write-Host Service $serviceName not found
+}
+
+# Check SX_Virtual_Link_Lite
+$serviceName = 'SX Virtual Link Lite'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    $status = Get-Service -Name $serviceName
+    #Write-Output $status
+
+    if ( $status.Status -ne "Running") {
+        Write-Output "Service $serviceName nor running"
+        Start-Service -DisplayName '$serviceName'
+    } else {
+        Write-Output "Service $serviceName is running nothig to do"
+    }
+} else {
+    Write-Host Service $serviceName not found
+}
+
+# Check UmRdpService
+$serviceName = 'UmRdpService'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    $status = Get-Service -Name $serviceName
+    #Write-Output $status
+
+    if ( $status.Status -ne "Running") {
+        Write-Output "Service $serviceName nor running"
+        Start-Service -DisplayName '$serviceName'
+    } else {
+        Write-Output "Service $serviceName is running nothig to do"
+    }
+} else {
+    Write-Host Service $serviceName not found
+}
+
+# Check ADSync
+$serviceName = 'ADSync'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    $status = Get-Service -Name $serviceName
+    #Write-Output $status
+
+    if ( $status.Status -ne "Running") {
+        Write-Output "Service $serviceName nor running"
+        Start-Service -DisplayName '$serviceName'
+    } else {
+        Write-Output "Service $serviceName is running nothig to do"
+    }
+} else {
+    Write-Host Service $serviceName not found
+}
+
+# Check AzureADConnectHealthSyncInsights
+$serviceName = 'AzureADConnectHealthSyncInsights'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    $status = Get-Service -Name $serviceName
+    #Write-Output $status
+
+    if ( $status.Status -ne "Running") {
+        Write-Output "Service $serviceName nor running"
+        Start-Service -DisplayName '$serviceName'
+    } else {
+        Write-Output "Service $serviceName is running nothig to do"
+    }
+} else {
+    Write-Host Service $serviceName not found
+}
+
+# Check AzureADConnectHealthSyncMonitor
+$serviceName = 'AzureADConnectHealthSyncMonitor'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    $status = Get-Service -Name $serviceName
+    #Write-Output $status
+
+    if ( $status.Status -ne "Running") {
+        Write-Output "Service $serviceName nor running"
+        Start-Service -DisplayName '$serviceName'
+    } else {
+        Write-Output "Service $serviceName is running nothig to do"
+    }
+} else {
+    Write-Host Service $serviceName not found
+}
+
+# Check Gdmms
+$serviceName = 'Gdmms'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    $status = Get-Service -Name $serviceName
+    #Write-Output $status
+
+    if ( $status.Status -ne "Running") {
+        Write-Output "Service $serviceName nor running"
+        Start-Service -DisplayName '$serviceName'
+    } else {
+        Write-Output "Service $serviceName is running nothig to do"
+    }
+} else {
+    Write-Host Service $serviceName not found
+}
+
+# Check PostgreSQL 11.0-1 Server
+$serviceName = 'PostgreSQL 11.0-1 Server'
 
 If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
     $status = Get-Service -Name $serviceName
