2d5d6c1971b3bdb2679b90497d96ad87b18119e8
Markus Sachs <markus.sachs@ametras.com>
2023-04-29 11:16:41 +0200

    Add new Script

--- update-git.ps1
+++ update-git.ps1
@@ -2,3 +2,4 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/MarkusSachs/ninja-scripts/main/Check-Services-Tasks.ps1 -OutFile C:\am_Ninja_Scripts\Check-Services-Tasks.ps1
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/MarkusSachs/ninja-scripts/main/ts-profile-cleanup-download-90-days.ps1 -OutFile C:\am_Ninja_Scripts\ts-profile-cleanup-download-90-days.ps1
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/MarkusSachs/ninja-scripts/main/ts-profile-cleanup.ps1 -OutFile C:\am_Ninja_Scripts\ts-profile-cleanup.ps1
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/MarkusSachs/ninja-scripts/main/update-git.ps1 -OutFile C:\am_Ninja_Scripts\update-git.ps1
