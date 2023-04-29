48514d1971a1368737f17f210d01b345bfdd73d0
Markus Sachs <markus.sachs@ametras.com>
2023-04-29 08:50:48 +0200

    Initil Commit

---
+++ update-git.ps1
@@ -0,0 +1,4 @@
+#Download Scrips from git
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/MarkusSachs/ninja-scripts/main/Check-Services-Tasks.ps1 -OutFile C:\am_Ninja_Scripts\Check-Services-Tasks.ps1
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/MarkusSachs/ninja-scripts/main/ts-profile-cleanup-download-90-days.ps1 -OutFile C:\am_Ninja_Scripts\ts-profile-cleanup-download-90-days.ps1
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/MarkusSachs/ninja-scripts/main/ts-profile-cleanup.ps1 -OutFile C:\am_Ninja_Scripts\ts-profile-cleanup.ps1
